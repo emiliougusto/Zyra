4d41f32c3046249bd13d4a302ac30c74e04447a4
Emilio <emiliogremiocolorado@gmail.com>
2025-09-10 15:28:17 -0300

    Inicializando Projeto Zyra Spring

---
+++ src/test/java/com/zyra/zyra/ZyraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyra.zyra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
