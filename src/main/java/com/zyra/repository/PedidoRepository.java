6a4c15e8a455c6428f031f90da72bda83030030e
lgtesser <luis.gt12@aluno.ifsc.edu.br>
2025-09-17 14:45:56 -0300

    Adicionar PedidoRepository para gerenciar operações de pedidos

---
+++ src/main/java/com/zyra/repository/PedidoRepository.java
@@ -0,0 +1,8 @@
+package com.zyra.repository;
+
+import com.zyra.model.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+
+}
