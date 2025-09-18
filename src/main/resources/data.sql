d79566ed381cd6afd2a3ba4944d2a8b47f3ccb61
Luiz Henrique <lhmendes0911@gmail.com>
2025-09-18 16:11:59 -0300

    Atualizar configuração de segurança para restringir acesso ao registro de usuários e melhorar a criptografia de senhas; adicionar dados iniciais ao banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_usuario (id_usuario, ds_email, role_usuario, ds_senha) VALUES
+(1, 'usuario@email.com', 1, '$2a$10$AI3vjykqoZPuRSJLvl00zOJtLYC2TvikDGgKhvhRdPAHvLyu/TFce'),
+(2, 'admin@email.com', 0, '$2a$10$5UX8Et.YepfpbLLqzkOUZ.YtJtBwffmuajtRg7hjIXkVyp2NX.tPS');
+
+INSERT INTO tb_produto (id_produto, nm_produto, ds_produto, vl_produto) VALUES
+(1, 'Camisa Nike', 'Camisa básica Nike branca', 129.90),
+(2, 'Calça Adidas', 'Calça Adidas preta', 149.90),
+(3, 'Tênis corrida', 'Tênis de corrida preto', 259.90);
